9f204022128d9b31cf6daf4891de07021fe7479b
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-04 14:19:26 -0700

    changed folders

--- part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/alien invasion/ship.py
+++ part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/alien invasion/ship.py
@@ -10,7 +10,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()   
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('part 2 projects\project 1 alien invastion\chapter 12 a ship that fires bullets\images\ship.bmp')
+        self.image = pygame.image.load('part 2 projects\project 1 alien invastion\chapter 12 a ship that fires bullets\alien invasion\images\ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
