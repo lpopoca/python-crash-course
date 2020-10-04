c73f8bc2da408129816078ff8416add2534479e0
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-04 15:19:13 -0700

    refactored the settings for the display and color

--- part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/exercises/12-1 blue sky/blue_sky.py
+++ part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/exercises/12-1 blue sky/blue_sky.py
@@ -5,12 +5,16 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class BlueSky:
     """Overall class to manage game assets and behavior."""
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((720, 1080))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
 
         # Set the background color.
         self.bg_color = (0, 0, 255)
@@ -24,7 +28,7 @@ class BlueSky:
                 sys.exit()
         
         # Redraw the screen during each pass through the loop.
-        self.screen.fill(self.bg_color)
+        self.screen.fill(self.settings.bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
---
+++ part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/exercises/12-1 blue sky/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all the settings for Blue Sky."""
+    def __init__(self):
+        """Initalize the games settings"""
+        #Screen settings
+        self.screen_width = 720
+        self.screen_height = 1080
+        self.bg_color = (0, 0, 255)
+        
