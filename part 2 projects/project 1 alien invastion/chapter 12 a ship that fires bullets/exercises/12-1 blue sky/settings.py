64264a38e93392afd67bb9372e149a9439e237c3
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-04 16:34:19 -0700

    Punctuation fix

--- part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/exercises/12-1 blue sky/settings.py
+++ part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/exercises/12-1 blue sky/settings.py
@@ -1,7 +1,7 @@
 class Settings:
     """A class to store all the settings for Blue Sky."""
     def __init__(self):
-        """Initalize the games settings"""
+        """Initalize the games settings."""
         #Screen settings
         self.screen_width = 720
         self.screen_height = 1080
