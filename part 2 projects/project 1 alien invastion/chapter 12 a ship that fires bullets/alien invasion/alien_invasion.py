4beee71ab73a77e0d03b115a09077cfa2fed305d
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-04 14:55:57 -0700

    refactored the code that updates bullets.

--- part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/alien invasion/alien_invasion.py
+++ part 2 projects/project 1 alien invastion/chapter 12 a ship that fires bullets/alien invasion/alien_invasion.py
@@ -25,12 +25,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            # Get rid of bullets that have disapeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -67,6 +62,17 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed: # 3 bullets max
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disapeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
