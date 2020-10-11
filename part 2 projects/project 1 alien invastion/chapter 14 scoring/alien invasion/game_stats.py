cd9163f5f0704a6fb191f56084138b7533cb410e
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-11 14:35:57 -0700

    Created highscore system.

--- part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/game_stats.py
+++ part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/game_stats.py
@@ -5,6 +5,8 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
+        # High score should never be reset.
+        self.high_score = 0
 
         # Start game in an inactive state.
         self.game_active = False
