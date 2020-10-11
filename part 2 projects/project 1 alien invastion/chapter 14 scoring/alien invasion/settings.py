69622598ef6f5050eb4651a938900f383307b627
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-11 13:37:43 -0700

    Game increases in difficulty with every clear.

--- part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/settings.py
+++ part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1080
         self.screen_height = 720
@@ -20,7 +20,24 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left.
+
+        # How quickly the game speeds up.
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # Fleet direction of 1 equals right; -1 represent left.
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
