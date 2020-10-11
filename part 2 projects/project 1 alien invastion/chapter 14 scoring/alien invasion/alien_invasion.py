33d64d1c8da2cf3a996d125518e7496992ae127b
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-10 19:19:02 -0700

    Play button resets stats and starts new game.

--- part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/alien_invasion.py
+++ part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/alien_invasion.py
@@ -57,6 +57,26 @@ class AlienInvasion:
 
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the play clicks play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset the game statistics.
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+            
     
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
