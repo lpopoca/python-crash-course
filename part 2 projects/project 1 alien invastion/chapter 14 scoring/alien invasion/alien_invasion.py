c16af5930443fe6daea1b8c3c646501e6fb13023
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-10 21:09:10 -0700

    Made cursor disapear when game starts.

--- part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/alien_invasion.py
+++ part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/alien_invasion.py
@@ -78,6 +78,8 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
     
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -199,6 +201,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
