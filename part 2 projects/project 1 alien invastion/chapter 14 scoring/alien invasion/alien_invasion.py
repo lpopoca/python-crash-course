88d5daf1d57bebef42c5538bfb13890a8b536464
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-11 14:48:29 -0700

    Implemented levels score.

--- part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/alien_invasion.py
+++ part 2 projects/project 1 alien invastion/chapter 14 scoring/alien invasion/alien_invasion.py
@@ -73,6 +73,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -138,6 +139,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update the positions
