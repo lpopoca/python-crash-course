9fa266704cb5eb8d4d934e917e44024b3256ec05
Leonardo Popoca <tabsz.github@outlook.com>
2020-10-26 20:32:46 -0700

    Created the plot for 5000 cubed numbers.

--- part 2 projects/project 2 data visualization/chapter 15 generating data/exercises/15-1 cubes/cubes.py
+++ part 2 projects/project 2 data visualization/chapter 15 generating data/exercises/15-1 cubes/cubes.py
@@ -3,7 +3,8 @@
 import matplotlib.pyplot as plt
 
 # Create the values
-x_values = [1,2,3,4,5]
+# x_values = [1,2,3,4,5] old values
+x_values = range(1, 5001)
 y_values = [x**3 for x in x_values]
 
 # Style
@@ -11,7 +12,7 @@ plt.style.use('seaborn')
 
 # Define the plot
 fig,ax = plt.subplots()
-ax.scatter(x_values,y_values,s=100)
+ax.scatter(x_values,y_values,s=10)
 
 # Set the chart title and labes the axes.
 ax.set_title("Cubed Numbers",fontsize=24)
@@ -20,6 +21,9 @@ ax.set_ylabel("Cubed Values",fontsize=14)
 
 # Set the size of the tick labels.
 ax.tick_params(axis='both',which='major',labelsize=14)
+
+# Set the range for each axis.
+ax.axis([0,125,0,2000000])
 
 # Show the plot
 plt.show()
